e66928fbfc4e615b65a4ce8641c119b3f0d48a7e
LConst01 <cunhalucca26@gmail.com>
2025-07-14 16:26:22 -0300

    Add files via upload

--- 02_exercicio.py
+++ 02_exercicio.py
@@ -1,23 +1,53 @@
 while True:
     nome = input("Digite o nome do usuário: ")
 
-    if len(nome) <= 2:
-        print("Nome deve ter três caractéres ou mais!") 
+    if len(nome) >= 3:
+        break
     else:
-        idade = int(input("Digite a sua idade: "))
-        if idade > 150:
-            print("Idade deve ser menor que 150!")
-        else:
-            salario = float(input("Digite o seu salário: "))
-            if salario == 0:
-                print("Salalário deve ser maior que zero!")
-            else:
-                generos = ['F', 'f', 'M', 'm', 'O', 'o']
-                genero = input("Digite seu gênero. 'M' para msaculino, 'F' para feminino e  'O' para outro: ")
-                if genero != generos:
-                    print("Digite o gênero novamente!")
-                else:
-                    break
-            
-    
-    
+        print("Nome deve ter três caractéres ou mais!") 
+        
+while True:
+    idade = int(input("Digite a sua idade: "))
+    if idade <= 150 and idade >= 0:
+        break        
+    else:
+        print("Idade deve ser menor que 150!")
+        
+while True:
+    salario = float(input("Digite o seu salário: "))
+    if salario > 0:
+        break 
+    else:
+        print("Salalário deve ser maior que zero!")
+        
+while True:
+    genero = input("Digite seu gênero. 'M' para msaculino, 'F' para feminino e  'O' para outro: ").upper()
+    if genero in ['M', 'F', 'O']:
+        break
+    else:
+        print("Digite o gênero novamente!")
+        
+while True:
+    estado_civil = input("Digite seu estado civil. 'S' para soltero, 'C' para casado, 'V' para viúvo, 'D' para divorcido:").upper()
+    if estado_civil in ['S', 'C', 'V', 'D']:
+        break
+    else:
+        print("Estado Civil inválido. Digite novamente!")
+
+print(f"Nome: {nome}.")
+print(f"Idade: {idade}.")
+print(f"Salário: R${salario:.2f}.")
+if genero == 'M':
+    print("Gênero: Masculino.")
+elif genero == 'F':
+    print("Gênero: Feminino.")
+elif genero == 'O':
+    print("Gênero: Outro.")
+if estado_civil == 'S':
+    print("Estado Civil: Solteiro.")
+elif estado_civil == 'C':
+    print("Estado Civil: Casado.")
+elif estado_civil == 'D':
+    print("Estado Civil: Divociado.") 
+elif estado_civil == 'V':
+    print("Estado Civil: Viúvo.")      
